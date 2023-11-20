d1ccd65dfa91de9ec3fbe16a779c9d6efaf4663e
Asteroid <aadrijamaitydps@gmail.com>
2023-11-20 15:33:24 +0530

    started

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import logo from "./images/logo.svg";
+
 function App() {
   return (
     <main>
-      <h1>Hello cats!</h1>
+      <div className="mx-12 my-10">
+        <img src={logo} alt="" className="block mx-auto my-6" />
+        <h1 className="text-4xl text-center font-light my-6 text-gray leading-loose">
+          We are launching soon!
+        </h1>
+      </div>
     </main>
   );
 }
